77d3d9314bd8021ca73f52a3f84c05ff3229a3ff
Joel Myers <joeldmyers@gmail.com>
2018-09-06 00:18:13 -0400

    first routing try

---
+++ pages/about.jsx
@@ -0,0 +1,8 @@
+const About = () => (
+  <section>
+    <h1>About Next JS</h1>
+    <p>Lorem Ipsum...</p>
+  </section>
+);
+
+export default About;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,13 @@
-const Index = () => <h1>Hello world from Next.js</h1>;
+import React from 'react';
+import Link from 'next/link';
 
+const Index = () => (
+  <section>
+    <h1>Hello world from Next.js</h1>
+    <Link href="/about">
+      <a>About This Project</a>
+    </Link>
+  </section>
+);
 
 export default Index;
